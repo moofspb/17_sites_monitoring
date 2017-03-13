30bd58437bf5465ffda204819e3a73d6a48803d9
Mikhail Kuntsevich <moofspb@gmail.com>
2017-03-14 01:23:43 +0300

    Add requirements.txt, add in check_sites_health.py functions load_urls4check, is_server_respond_with_200 and get_domain_expiration_date

--- check_sites_health.py
+++ check_sites_health.py
@@ -1,11 +1,43 @@
+import os
+import requests
+import whois
+from datetime import datetime
+
+
 def load_urls4check(path):
-    pass
+    if not os.path.exists(path):
+        return None
+    with open(path, 'r') as handler:
+        content = handler.readlines()
+        return [url.rstrip('\n') for url in content]
+
 
 def is_server_respond_with_200(url):
-    pass
+    try:
+        request = requests.get(url)
+        return True if request.status_code == 200 else False
+    except requests.exceptions.ConnectionError:
+        return False
+
 
 def get_domain_expiration_date(domain_name):
-    pass
+    try:
+        domain_info = whois.query(domain_name)
+        return domain_info.expiration_date
+    except AttributeError:
+        return None
+
 
 if __name__ == '__main__':
-    pass
+    urls = load_urls4check('urls.txt')
+    domains = [domain.lstrip('http://') for domain in urls]
+    for url in urls:
+        if is_server_respond_with_200(url):
+            print('{} - OK!'.format(url))
+        else:
+            print('{} - ERROR!'.format(url))
+    for domain in domains:
+        if get_domain_expiration_date(domain)
+
+
+
